7a93319dd177b68e7c0cffe44ec244a3b2dc52f3
Guilherme Vilã <guilhermesnts.profissional@gmail.com>
2025-03-24 14:00:20 -0300

    aaa

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 class Cliente {
-    constructor(nome, cpf, agencia, saldo) {
+    constructor(nome, cpf) {
         this.nome = nome;
         this.cpf = cpf;
     }
@@ -10,10 +10,15 @@ class ContaCorrente {
         this.agencia = agencia;
         this.saldo = saldo;
     }
+    exibirSaldo() {
+        console.log(`Seu saldo é de ${this.saldo}`);
+    }
 }
 
 const cliente1 = new Cliente("Joao", 11122233309);
 const cliente2 = new Cliente("Maria", 55544477723);
+const contacorrente1 = new ContaCorrente(1001, 500);
+
+contacorrente1.exibirSaldo();
 
 console.log(`Cliente 1 tem o nome de ${cliente1.nome}`); /* o .nome é o primeiro this.nome, o = nome serve apenas para dizer que o this.nome corresponde ao primeiro paramentro da classe cliente */ 
-cliente1.exibirsaldo();
